5e3c8743f8b78742b1eb9ececb615b6b2d8db1db
Sanssin <nurihsanudin13@gmail.com>
2026-06-25 14:03:39 +0700

    tambah nama pengembang

--- pltn_video_display/credits_data.py
+++ pltn_video_display/credits_data.py
@@ -2,7 +2,10 @@ CREDITS = [
     {"section": "Dosen Pembimbing", "names": ["Prof. Dr. Anhar Riza Antariksawan, DEA"]},
     {"section": "Special Project Tahun 2025", "names": [
         "Daffa Arkhan Baihaqi", "Melani Sistya Ardhana",
-        "Shelly Zahra Nidyaputri", "Wisnu Fitra Rachman"
+        "Shelly Zahra Nidyaputri", "Wisnu Fitra Rachman",
+	"Cahya Pribadi Mulya", "Aprilia Cahya Maharani", "Rahmat Fajar Hidayatullah",
+	"Nadira Vasya Salsabila"
+
     ]},
     {"section": "Pengabdian Masyarakat Tahun 2025", "names": [
         "Nur Ihsanudin", "Muhammad Elwas Yusuf",
@@ -11,9 +14,9 @@ CREDITS = [
     {"section": "Pekan Kreativitas Mahasiswa Tahun 2025", "names": [
         "Muhammad Elwas Yusuf", "Maulida Yusfarina Rahma", "Muhammad Nurrosyid Pangestu",
         "Alfina Khairani", "Kinanthi Tara Salwahita", "Rabbany Al-Malika I",
-        "Hanifah Nurul Aqila", "Mohammad Irgi Islami", "Kunti Aisyatuzzahra",
+        "Hanifah Nurul Aqila", "Mohammad Irgi Islami","Nur Ihsanudin", "Kunti Aisyatuzzahra",
         "Pasha Octa Perdana", "Faljarisy Bellyn", "Syamuel Libaas A. A.",
-        "Nur Ihsanudin", "Aisya Zhavira Pharamesty", "Ni Mas Aqila Najwan",
+        "Aisya Zhavira Pharamesty", "Ni Mas Aqila Najwan",
         "Eka Risky Herdiansyah", "Rahmaningrum", "Shafina Zaidiya Nihrira",
         "Muhammad Athariq Sonitia", "Bilbina Balqis", "Ganazel Oktaviano R.",
         "Gathfan Darmawan", "Rasya Andrew Budi S.", "Rizkiana Ramadhani"
