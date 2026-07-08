1fe9ac43a730e55b94cc2fdf34e0bf72ebc3fe76
Sanssin <nurihsanudin13@gmail.com>
2026-07-08 18:28:26 +0700

    Reduce max servo angle to prevent control rod from going out of bounds

--- raspi_central_control/controllers/servo_controller.py
+++ raspi_central_control/controllers/servo_controller.py
@@ -16,7 +16,7 @@ except ImportError:
 class ServoController:
     # Typical standard servo pulse widths (microseconds)
     MIN_PW = 500   # 0 degrees
-    MAX_PW = 2478  # 178 degrees (reduced from 2500/180deg to prevent rod breakage)
+    MAX_PW = 2100  # ~144 degrees (reduced from 2478 to prevent rod from going out of bounds and failing to SCRAM)
     
     def __init__(self, safety_pin=23, shim_pin=24, reg_pin=25):
         """
